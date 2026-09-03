37a47fd6427a2dea8864ab51a87f96a7a7d95722
izquierdodie <diegoizquierddo@gmail.com>
2026-09-03 19:05:42 -0300

    Revert "Commit practica"
    This reverts commit 8d3a05a1a4dc61116944dc639e3e7f61cb5607e5.

--- main.py
+++ main.py
@@ -9,5 +9,3 @@ numero = int(input("Ingrese un numero: "))
 numero2 = int(input("Ingrese otro numero: "))
 
 print(f"{nombre}, la suma de los numeros es: {numero + numero2}.")
-
-print("Cambio para poder subir el commit xd.")
